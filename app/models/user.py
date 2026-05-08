d12062c0796db3db0d1b104036d346cb017b8381
Nomsolives <nomsoosuego@gmail.com>
2026-05-08 23:34:15 +0100

    Create user.py

---
+++ app/models/user.py
@@ -0,0 +1,24 @@
+from app import db
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    name = db.Column(db.String(100))
+
+    email = db.Column(
+        db.String(120),
+        unique=True
+    )
+
+    password = db.Column(db.String(255))
+
+    role = db.Column(db.String(50))
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "email": self.email,
+            "role": self.role
+        }
